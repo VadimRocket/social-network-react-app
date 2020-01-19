14ef1ad0f5f82024ad0f9d306e010e4499b97d4b
Vadim Martin <fragariastar@gmail.com>
2020-01-19 07:46:23 +0300

    1

--- src/storage/reducers/users_reducer.js
+++ src/storage/reducers/users_reducer.js
@@ -7,7 +7,7 @@ let initialState = {
         { id: 1, status: 'active', followed: false, location: {city: 'Tokyo', country:'Japan'}, name:'Oliver', photo: 'http://avotarov.net/picture/avatar-100/kartinki/903.jpg' },
         { id: 2, status: 'active', followed: true, location:  {city: 'Berlin', country:'Germany'}, name:'Jack', photo: 'http://avotarov.net/picture/avatar-100/kartinki/905.jpg' },
         { id: 3, status: 'active', followed: true,  location: {city: 'Paris', country:'France'},  name:'Harry', photo: 'http://avotarov.net/picture/avatar-100/kartinki/902.jpg' },
-        { id: 4, status: 'active', followed: false, location: {city: 'Finland', country:'Akaa'},  name:'Alex', photo: 'http://avotarov.net/picture/avatar-100/kartinki/910.jpg' },
+        { id: 4, status: 'active', followed: false, location: {city: 'Akka', country:'Finland'},  name:'Alex', photo: 'http://avotarov.net/picture/avatar-100/kartinki/910.jpg' },
     ]
 };
 
