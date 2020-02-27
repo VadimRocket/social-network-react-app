21f440e9128f121e1fee8a1a4cb2aff8bde6682b
Vadim Martin <fragariastar@gmail.com>
2020-02-28 02:26:09 +0300

    form validation

--- src/components/Common/FormControls/FormControls.js
+++ src/components/Common/FormControls/FormControls.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import s from './FormControls.module.css';
 
-export const Textarea = ({input, meta, ...props}) => { // props будут содержать все кроме input, meta исп rest operator
-
+const FormControl = ({input, meta, child, ...props}) =>  {
+    
     const hasError = meta.touched && meta.error;
     return (
-        <div className={s.formControl + ' ' +  ( hasError ? s.error : '' )}>
-             <textarea {...input} {...props}/> 
+        <div className={ `${s.formControl} ${hasError ? s.error : '' } `}>
+               {props.children}   
                {/*if touched and no data return error */}
                { hasError && <span>{meta.error}</span> } 
         </div>
     )
 }
+// wrappers: Textarea and Input  
+export const Textarea = (props) => { 
+    const {input, meta, child, ...restProps} = props;
+    return <FormControl {...props}> <textarea {...props.input} {...restProps} /></FormControl>
+}
 
-// need  refactoring
-export const Input = ({input, meta, ...props}) => { 
-
-    const hasError = meta.touched && meta.error;
-    return (
-        <div className={s.formControl + ' ' +  ( hasError ? s.error : '' )}>
-             <input {...input} {...props}/> 
-               {/*if touched and no data return error */}
-               { hasError && <span>{meta.error}</span> } 
-        </div>
-    )
+export const Input = (props) => { 
+    const {input, meta, child, ...restProps} = props;
+    return <FormControl {...props}>  <input {...input} {...restProps} /></FormControl>
 }
---
+++ src/components/Common/FormControls/FormControlsNoChild.js
@@ -0,0 +1,27 @@
+// import React from 'react';
+// import s from './FormControls.module.css';
+
+// export const Textarea = ({input, meta, ...props}) => { // props будут содержать все кроме input, meta исп rest operator
+
+//     const hasError = meta.touched && meta.error;
+//     return (
+//         <div className={s.formControl + ' ' +  ( hasError ? s.error : '' )}>
+//              <textarea {...input} {...props}/> 
+//                {/*if touched and no data return error */}
+//                { hasError && <span>{meta.error}</span> } 
+//         </div>
+//     )
+// }
+
+// // need refactoring
+// export const Input = ({input, meta, ...props}) => { 
+
+//     const hasError = meta.touched && meta.error;
+//     return (
+//         <div className={s.formControl + ' ' +  ( hasError ? s.error : '' )}>
+//              <input {...input} {...props}/> 
+//                {/*if touched and no data return error */}
+//                { hasError && <span>{meta.error}</span> } 
+//         </div>
+//     )
+// }
