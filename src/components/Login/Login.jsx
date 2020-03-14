a33867201ed0f496b1a5df778d6a5bb99e345a3e
Vadim Martin <fragariastar@gmail.com>
2020-03-14 20:40:08 +0300

    deleted old login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,52 +2,37 @@ import React from "react";
 import s from "./Login.module.css";
 import style from "../Common/FormControls/FormControls.module.css";
 import { reduxForm } from "redux-form";
-// import {Field} from 'redux-form';
 import { required } from "../../utils/validators/validators";
 import { Input, createFieldFC } from "../Common/FormControls/FormControls";
 import { connect } from "react-redux";
 import { login } from "../../storage/reducers/auth-reducer";
 import { Redirect } from "react-router-dom";
 import Button from "../Common/Button/Button";
-import { withAuthRedirect } from "../../hoc/withAuthRedirect";
-import { compose } from "redux";
 
 const LoginForm = ({ handleSubmit, error, captcha }) => {
-   console.log(props);
    return (
       <>
          {/* // see createFieldFC} from '../Common/FormControls/FormControls */}
-         {/* Field контейнерная компонента которая рисует другую компоненту приходит из redux-form */}
+         {/*!!!!! createFieldFC(placeholder, name, component, validators, props = {}, text= '') */}
          <div className={s.formContainer}>
             <form onSubmit={handleSubmit} className={s.uiForm}>
                <div className={s.formRow}>
-                  {/*!!!!! createFieldFC(placeholder, name, component, validators, props = {}, text= '') */}
-
                   {createFieldFC("Email", "email", Input, [required])}
-                  {/* <Field  placeholder={'Email'} name={'email'} component={Input} validate={required} />*/}
                </div>
                <div className={s.formRow}>
                   {createFieldFC("Password", "password", Input, [required], {
                      type: "password"
                   })}
-                  {/* <Field  placeholder={'Password'}  name={'password'} type={'password'} component={Input} validate={required}  /> */}
                </div>
+               {/* Remember me checkbox*/}
+               {createFieldFC(null, "rememberMe", Input, [required], {
+                  type: "checkbox"
+               })}
 
-               {/* Remember me */}
-               {createFieldFC(
-                  null,
-                  "rememberMe",
-                  Input,
-                  [required],
-                  { type: "checkbox" },
-                  ""
-               )}
-
+               {/* captcha */}
                {captcha && <img src={captcha} alt='captcha' />}
-               {/*!!!!! createFieldFC(placeholder, name, component, validators, props = {}, text= '') */}
                {captcha &&
                   createFieldFC("Symbols", "captcha", Input, [required], {})}
-               {/* <Field  className={s.formRow} component={Input}  name={'rememberMe'} type={'checkbox'} validate={required}  /> */}
                {/* common error for the form */}
                {error && <div className={style.formSummaryError}>{error}</div>}
                <Button name={"Sign in"} />
@@ -63,8 +48,7 @@ const LoginReduxForm = reduxForm({
 })(LoginForm);
 // store.getState().form
 
-const Login = ({ isAuth, login, captcha }) => {
-   //   console.log(props);
+const Login = ({ login, isAuth, captcha }) => {
    // сюда придут все значения из формы
    const onSubmit = formData => {
       login(
@@ -87,4 +71,9 @@ const Login = ({ isAuth, login, captcha }) => {
    );
 };
 
-export default Login;
+const mapStateToProps = state => ({
+   captcha: state.auth.captcha,
+   isAuth: state.auth.isAuth
+});
+
+export default connect(mapStateToProps, { login })(Login);
