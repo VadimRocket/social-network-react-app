c7da3079fb5cf7eff9660ea979b6710ec3565a28
Vadim Martin <fragariastar@gmail.com>
2020-07-08 11:24:47 +0400

    updated avatar

--- src/storage/reducers/sidebar_reducer.js
+++ src/storage/reducers/sidebar_reducer.js
@@ -4,7 +4,7 @@ let initialState = {
 
     friends: [
         { id: 1, name: 'Alex',    photo: 'https://www.discordavatars.com/wp-content/uploads/2020/07/marvel-character-avatar-188.jpg' },
-        { id: 2, name: 'Olivia',  photo: 'https://www.discordavatars.com/wp-content/uploads/2020/05/male-actors-avatar-004.jpg' },
+        { id: 2, name: 'Olivia',  photo: 'https://www.discordavatars.com/wp-content/uploads/2020/05/rap-artist-avatar-047.jpg' },
         { id: 3, name: 'Harry',   photo: 'https://www.discordavatars.com/wp-content/uploads/2020/05/444979733181235210.jpg' },
         { id: 4, name: 'Charlie', photo: 'https://www.discordavatars.com/wp-content/uploads/2020/05/the-walking-dead-avatar-002.jpg' },
         { id: 5, name: 'Jack',    photo: 'https://www.discordavatars.com/wp-content/uploads/2020/05/male-actors-avatar-028.jpg' },
